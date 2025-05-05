035f0609d898a06d21feaaa73daf6190d536654b
MandeepBasnet <mandeep3basnet@gmail.com>
2025-05-05 16:23:35 +0545

    Fixing necessary database renaming

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 -- Create the LawLink database
-CREATE DATABASE IF NOT EXISTS c5_db;
-USE c5_db;
+CREATE DATABASE IF NOT EXISTS lawlink;
+USE lawlink;
 
 -- Create Users table (base table for all user types)
 CREATE TABLE IF NOT EXISTS Users (
